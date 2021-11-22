ca2dd895459450f5bda39260b9db1f73db36aae3
Riola <riolalama@live.com>
2021-11-22 10:13:50 +0100

    Js Scripts

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -3,7 +3,7 @@ const key = 'AxJxIEWHsA4SzuCsf2Q1zWApvYvAM6Kc';
 //get Weather Information
 const getWeather = async (id) => {
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
     const response = await fetch(base + query);
     const data = await response.json();
@@ -12,7 +12,7 @@ const getWeather = async (id) => {
 
 //get City Information
 const getCity = async (city) => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`
 
 
